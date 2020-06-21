913edd7eec4e8fa70b1d0aa9402717f2e2e5009e
carloscruz85 <carloscruz85@gmail.com>
2020-06-21 13:32:24 -0600

    video banner pc done

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.scss";
 import Banner from "../../components/main_banner";
 import TextBanner from "../../components/textBanner";
+import VideoBanner from "../../components/videoBanner";
 
 const App = () => {
   return (
     <>
-      <Banner />
-      <TextBanner />
+      <VideoBanner />
+      {/* <Banner />
+      <TextBanner /> */}
     </>
   );
 };
